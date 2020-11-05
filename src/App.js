2aed2f22bec4b4e32dc9326550e2b587697b1a7b
Meher Chandan <meherchandan12@gmail.com>
2020-11-06 01:22:08 +0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Row from './Row';
+import requests from './requests';
+
+function App() {
+  return (
+    <div className="App">
+     <Row title = "NETFLIX ORIGINALS" fetchUrl = {requests.fetchNetflixOriginals}/>
+     <Row title = "Trending Now" fetchUrl = {requests.fetchTrending}/>
+    </div>
+  );
+}
+
+export default App;
