2a7ef135fe45dd7d93d0a5f0be8ed02c1c93d447
Meher Chandan <meherchandan12@gmail.com>
2020-11-07 01:24:48 +0400

    adding firebase files

--- src/Row.js
+++ src/Row.js
@@ -27,7 +27,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
     if (trailerUrl) {
       setTrailerUrl("");
     } else {
-      movieTrailer(movie?.name || movie.title || "")
+      movieTrailer(movie?.name || movie.title || movie?.original_name || "")
         .then((url) => {
           const urlParams = new URLSearchParams(new URL(url).search);
           setTrailerUrl(urlParams.get("v"));
