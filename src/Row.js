579cac30053ec4d3fa1fe67b65af01d8153f30c5
Meher Chandan <meherchandan12@gmail.com>
2020-11-06 23:45:39 +0400

    added Banner and Row for movies

--- src/Row.js
+++ src/Row.js
@@ -1,36 +1,36 @@
-import React, {useState, useEffect} from 'react'
-import axios from './axios';
-import './Row.css';
-const baseUrl = "https://image.tmdb.org/t/p/original"
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
+const baseUrl = "https://image.tmdb.org/t/p/original";
 
-function Row({ title, fetchUrl }) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        async function fetchData() {
-        const request = await axios.get(fetchUrl); 
-        setMovies( request.data.results);
-        console.table(request.data.results)
-        }
-        fetchData();
-    }, [fetchUrl]);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+    }
+    fetchData();
+  }, [fetchUrl]);
 
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row_posters">
-            {
-                movies.map(movie=>(
-                    <img 
-                    key={movie.id}
-                    className="row_poster"
-                    src={`${baseUrl}${movie.poster_path} `} 
-                    alt={movie.name} />
-                ) )
-            }
-            </div>
-        </div>
-    )
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseUrl}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            } `}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
